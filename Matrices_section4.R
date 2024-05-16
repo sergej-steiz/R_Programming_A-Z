0ff396fd93f6c33f8ceda8a63dbd876d2f83ead7
Sergius Steiz <sergejsteiz@gmx.de>
2024-05-16 07:10:21 +0200

    status update

--- Matrices_section4.R
+++ Matrices_section4.R
@@ -1,3 +1,18 @@
 # Matrices
 typeof(Salary)
+Salary
+Games
+MinutesPlayed
+# matrix() - like a snake , fill in column1 column2 ..
+# rbind()  - vectors (same length) are binned row by row 
+# cbind()  - vectors (same length) are binned column by colums
+?matrix
+my.data <- 1:20
 
+A <- matrix(my.data, nrow=4, ncol=5)
+A
+A[2,3]
+
+B <- matrix(my.data, nrow=4, ncol=5, byrow = TRUE)
+B
+B[2,5]
