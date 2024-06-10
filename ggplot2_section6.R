20f979d4ba34464927ef4477def03ef89f574be4
Sergius Steiz <sergejsteiz@gmx.de>
2024-06-10 07:22:30 +0200

    ggplot started

--- ggplot2_section6.R
+++ ggplot2_section6.R
@@ -26,3 +26,29 @@ movies$Years <- factor(movies$Year)
 summary(movies)
 str(movies)
 
+#----------------------------------- Aesthetics
+library(ggplot2)
+
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating))
+
+# add geometry
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating)) + 
+  geom_point()
+# add colour
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating
+                          , colour=Genre)) + 
+  geom_point()
+
+# add size
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating
+                          , colour=Genre, size=Genre)) + 
+  geom_point()
+
+# add size - better way
+ggplot(data = movies, aes(x=CriticRating, y=AudienceRating
+                          , colour=Genre, size=BudgetMillions)) + 
+  geom_point()
+#>> This is #1 (we will improve it)
+#>
+
+
